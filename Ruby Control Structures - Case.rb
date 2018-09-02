68a6adb0e36c92ec10da29f3b7cd03310a732bdc
valeriaragonese <ribesnero80@gmail.com>
2018-09-02 16:41:39 +0100

    Solution
    Completed

--- Ruby Control Structures - Case.rb
+++ Ruby Control Structures - Case.rb
@@ -1,4 +1,14 @@
-loop do
-    coder.practice
-    break if coder.oh_one? == true
-end 
+def identify_class(obj)
+    case obj
+        when Hacker
+        puts "It's a Hacker!"
+        when Submission
+        puts "It's a Submission!"
+        when TestCase
+        puts "It's a TestCase!"
+        when Contest
+        puts "It's a Contest!"
+        else
+        puts "It's an unknown model"
+        end
+end
