3be2f2fa48e1beca2d56eb4c7196cab18fc5bcfb
valeriaragonese <ribesnero80@gmail.com>
2018-09-25 23:28:12 +0100

    complete

---
+++ 30DaysOfCode:D1 - Data Type.rb
@@ -0,0 +1,16 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+b = 12
+e = 4.0
+tr = "is the best place to learn and practice coding!"
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+puts i + b
+# Print the sum of the double variables on a new line.
+puts d + e
+# Concatenate and print the String variables on a new line
+puts s + tr
+# The 's' variable above should be printed first.
